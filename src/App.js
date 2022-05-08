46609a24ed9fe6f394df5bfe5c6ac877478530c8
H. alrahmani <h.m.alrahmani@gmail.com>
2022-05-08 20:37:07 +0300

    COMPLETED task 1 & almost task 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import products from "./products.js";
+//maybe?
+function Products(props) {
+  return (
+    <div>
+      <img src={props.img} alt="a book cover"></img>
+      <h2>{props.name}</h2>
+      <h6>{props.price}</h6>
+    </div>
+  );
+}
 function App() {
+  const productList = products.map((product) => (
+    <Products name={product.name} price={product.price} img={product.img} />
+  ));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Corner Bookshop</h1>
+      <h3>A bookworm's heaven</h3>
+      <img
+        src="https://citymonitor.ai/wp-content/uploads/sites/3/2021/02/sergiu-valena-698563-unsplash-658x370.jpg"
+        alt="A Cozy Bookstore"
+        width="100%"
+        height="650"
+      ></img>
+      {productList}
     </div>
   );
 }
---
+++ src/products.js
@@ -0,0 +1,14 @@
+const products = [
+  {
+    name: "Sherlock Holmes",
+    price: 15,
+    img: "https://i.pinimg.com/originals/f6/9a/d4/f69ad46f33a5ce68edcbe52801629cd6.jpg",
+  },
+  {
+    name: "The Great Gatsby",
+    price: 12,
+    img: "http://toptenbooks.net/sites/default/files/Gatsby.jpg",
+  },
+];
+
+export default products;
